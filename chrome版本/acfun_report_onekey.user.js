b80aac7503d131dda4f99a0b04d409ee556e3255
Sinyupl <yangwangchao@gmail.com>
2016-05-05 11:28:57 +0800

    Update acfun_report_onekey.user.js

--- chrome版本/acfun_report_onekey.user.js
+++ chrome版本/acfun_report_onekey.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
-// @author       You
+// @author       Sinyupl
 // @match        http://www.acfun.tv/*
 // @match        http://acfun.tudou.com/v/*
 // @grant        none
